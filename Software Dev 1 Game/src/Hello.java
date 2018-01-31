ccab91292d1238e49a8711fba1248f7ecc799b0b
SirNayfinn <32822074+SirNayfinn@users.noreply.github.com>
2018-01-30 22:36:22 -0500

    sfsdfsd

--- Software Dev 1 Game/src/Hello.java
+++ Software Dev 1 Game/src/Hello.java
@@ -6,20 +6,14 @@
 public class Hello
 {
 
-	/**
-	 * 
-	 */
 	public Hello()
 	{
-		// TODO Auto-generated constructor stub
+		
 	}
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args)
 	{
-		// TODO Auto-generated method stub
+		
 
 	}
 
