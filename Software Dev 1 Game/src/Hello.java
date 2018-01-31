5d233c787ae7bf83946ea2c44994130e9aaaeff1
SirNayfinn <32822074+SirNayfinn@users.noreply.github.com>
2018-01-30 22:29:11 -0500

    Laptop push test 2

---
+++ Software Dev 1 Game/src/Hello.java
@@ -0,0 +1,26 @@
+/**
+ * Author: ----- Natanael Nistor
+ * Date: ----- Jan 30, 2018
+ */
+
+public class Hello
+{
+
+	/**
+	 * 
+	 */
+	public Hello()
+	{
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
