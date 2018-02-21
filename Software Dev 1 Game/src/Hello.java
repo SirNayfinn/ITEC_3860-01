9e5c720896d0d2df7c1e3fcda305609775f0fb79
SirNayfinn <32822074+SirNayfinn@users.noreply.github.com>
2018-02-21 11:10:08 -0500

    Nathan's class

--- Software Dev 1 Game/src/Hello.java
+++ Software Dev 1 Game/src/Hello.java
@@ -19,8 +19,6 @@ public class Hello
 		//Sean's comment
 		
 
-		
-
 		System.out.println("Hello.....");
 		
 		
---
+++ Software Dev 1 Game/src/NatanaelNistor.java
@@ -0,0 +1,11 @@
+
+public class NatanaelNistor
+{
+
+	public static void main(String[] args)
+	{
+		System.out.println("This is nathans class");
+
+	}
+
+}
